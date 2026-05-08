422cba44289780ffb5e75e0a3c835aff622fd134
JakeSurry <jacobsurry05@gmail.com>
2026-05-07 21:59:36 -0700

    Update cors address

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 setServers(["8.8.8.8", "8.8.4.4"]);
 
-app.use(cors({ origin: `http://localhost:5173`, credentials: true }));
+app.use(cors());
 
 app.use(express.json());
 app.use(configureSession());
